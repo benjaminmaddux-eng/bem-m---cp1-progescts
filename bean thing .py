ffdf8daf7e6d10ed1290102c2bf9f8723cb9b8c4
benjaminmaddux-eng <benjamin.maddux@ucas-edu.net>
2026-08-19 09:12:55 -0600

    bean thing .

---
+++ bean thing .py
@@ -0,0 +1,12 @@
+#bean J man first tthing
+
+
+
+name= input("Hi, hello whats you name?")
+print( "hello " + name )
+print("add number")
+var1=int(input("number: "))
+var2=int(input("number: "))
+var3=int(print(var1*var2))
+var4=print(+name +var3)
+print("your new user name is" +var4)
