2d1e0b84b2d2b62f4f24198adf333ab76beaefa4
benjaminmaddux-eng <benjamin.maddux@ucas-edu.net>
2026-08-19 09:13:57 -0600

    the first project

--- bean thing .py
+++ bean thing .py
@@ -2,11 +2,10 @@
 
 
 
-name= input("Hi, hello whats you name?")
+name= input("Hi, hello whats you name? ")
 print( "hello " + name )
 print("add number")
 var1=int(input("number: "))
 var2=int(input("number: "))
-var3=int(print(var1*var2))
-var4=print(+name +var3)
-print("your new user name is" +var4)
+var3=str(print(var1*var2))
+str(print(+name)print(+var3))
