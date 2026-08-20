6882b4f39b21dd638cf2ca6c4ea3af2895109a5a
benjaminmaddux-eng <benjamin.maddux@ucas-edu.net>
2026-08-20 09:06:57 -0600

    i tryed to add my main code file/tabto the commit and push thing if its not there dnt worry

---
+++ thing/var_project_aug_19th .py
@@ -0,0 +1,14 @@
+#var project
+
+
+name=input("input name=")
+
+
+print("sorry I ment  " + name)
+
+age = 14
+
+print("wow you are" ,age)
+print("oh sorry i missed your birthday what age are you now?")
+age +=1
+print("ohh so your" ,age)
