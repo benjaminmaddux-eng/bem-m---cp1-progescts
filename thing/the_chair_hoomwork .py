12b4ad1ea59ec39a2fc2bff3c712a9627f06d252
benjaminmaddux-eng <benjamin.maddux@ucas-edu.net>
2026-08-21 08:44:42 -0600

    maby work?

---
+++ thing/the_chair_hoomwork .py
@@ -0,0 +1,15 @@
+# the chair thing work for cp1 and name is benjamin maddux
+gold=15
+print("your gold" ,gold)
+name=input("whats your name=     ")
+age=input( "hi "+name+" how old are you?      ")
+work=input("nice what do you do for work?")
+origin=input("where do you come from travler?    ")
+print("service cost is 5 gold")
+gold= 15-5
+print("lets get you adventures profile set up")
+print (name) 
+print (age)
+print (work)
+print (origin)
+print ("current gold" , gold)
