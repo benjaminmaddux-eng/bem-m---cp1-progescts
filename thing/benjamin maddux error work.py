fe196541821ccbdd4e1ceb157a74696550958539
benjaminmaddux-eng <benjamin.maddux@ucas-edu.net>
2026-08-21 08:52:51 -0600

    i did the error work for the 25th

---
+++ thing/benjamin maddux error work.py
@@ -0,0 +1,7 @@
+#fix was the parentese where not closed
+# and the + nam + was missing the e at the end so it was in compleat
+
+name = "Alex"
+age = 16
+
+print("My name is " + name + " and I am " + str(age) + " years old.")
