f75e5641dd96df97ac9428003770fd39723b59b5
benjaminmaddux-eng <benjamin.maddux@ucas-edu.net>
2026-08-26 09:51:59 -0600

    i did something actualy usefull

--- username .py
+++ username .py
@@ -1,8 +1,42 @@
 #username thingy
 
 print("username maker")
-name=input("nickname= ")
-favnum1=input("number=")
-favnum2=input("number=")
-power=favnum1^favnum2
+name=input("nickname: ")
+while True:
+    try:
+        favnum1=int(input("number:"))
+    except:
+        print("try again")
+    else:
+        break
+while True:
+    try:
+        favnum2=int(input("number:"))
+    except:
+        print("try again")
+    else:
+        break
+power=int(favnum1*favnum2)
 print(name,power)
+while True:
+    password=input("9 didget password: ")
+
+    if len(password) !=9:
+        print("password to short")
+        continue   
+    
+    print("your password is now "+password)
+    break
+print("please confirm your password and username")
+good_pasword=password
+while True:
+    user_try=input("check your password:")
+
+    if user_try == good_pasword:
+        print("good job")
+        break
+    else:
+        print("try again")
+print("comleated user name and pasword")
+print(name,power)
+print(password)
