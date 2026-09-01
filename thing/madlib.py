1d17272e282a701ee3b6526005a2a06d7b8adc29
bean <benjamin.maddux@ucas-edu.net>
2026-09-01 18:24:19 +0000

    worked

--- thing/madlib.py
+++ thing/madlib.py
@@ -1,1 +1,13 @@
-print("the other day i" +verb1+ "and after that i had" food1 "for lunch it was very" reaction1 "after that i made my way to" place1 "to go" verb2 "after that exausting" verb2 " training ")
+ while True:
+    try: 
+        class1=str"grade for first period   "))
+    except:
+        print("not a number")
+    else:
+        break
+
+
+
+
+
+print("the other day i" +verb1+ "and after that i had" food1 "for lunch it was very"+reaction1+"after that i made my way to"+place1+"to go"+verb2 "after that exausting"+verb2+" training after that i got another"+food1+"before going to bed and i"+verb3
