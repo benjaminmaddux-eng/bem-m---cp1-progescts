e3efcbd39ef5a541b60053a87a99e6a891782cef
bean <benjamin.maddux@ucas-edu.net>
2026-09-02 15:37:32 +0000

    work?

--- thing/madlib.py
+++ thing/madlib.py
@@ -6,3 +6,4 @@ place1=str(input("place:   "))
 verb2=str(input("verb:   "))
 verb3=str(input("verb:   "))
 print("today i woke up and felt " +reaction1+ " that i had to " +verb1+ " after that i got some " +food1+ " and went to " +place1+ " to " +verb2+ " after that " +reaction1,verb2+ "ing i went home and " +verb3+ " to bed")
+#lets hope this works
