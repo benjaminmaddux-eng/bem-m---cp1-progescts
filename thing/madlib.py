221e799865036d213ec03f3be4457cceba068a52
benjaminmaddux-eng <benjamin.maddux@ucas-edu.net>
2026-09-02 09:32:17 -0600

    another thing for madlib work

--- thing/madlib.py
+++ thing/madlib.py
@@ -1,1 +1,8 @@
-print("the other day i" +verb1+ "and after that i had" food1 "for lunch it was very" reaction1 "after that i made my way to" place1 "to go" verb2 "after that exausting" verb2 " training ")
+#benjamin maddux ms larose period 1
+verb1=str(input("verb:   "))
+food1=str(input("food:   "))
+reaction1=str(input("emotion:   "))
+place1=str(input("place:   "))
+verb2=str(input("verb:   "))
+verb3=str(input("verb:   "))
+print("today i woke up and felt " +reaction1+ " that i had to " +verb1+ " after that i got some " +food1+ " and went to " +place1+ " to " +verb2+ " after that " +reaction1,verb2+ "ing i went home and " +verb3+ " to bed")
