7081e9866c66cded6958063bf2fbc85821798f96
benjaminmaddux-eng <benjamin.maddux@ucas-edu.net>
2026-09-02 09:51:10 -0600

    nots

---
+++ thing/notes.py
@@ -0,0 +1,23 @@
+#tip calculator
+bill=input("money here= ")
+tip_tax=15/100
+result=tip_tax % bill
+print(result)
+
+
+
+
+#var project
+
+
+name=input("input name=")
+
+
+print("sorry I ment  " + name)
+
+age = 14
+
+print("wow you are" ,age)
+print("oh sorry i missed your birthday what age are you now?")
+age +=1
+print("ohh so your" ,age)
