9e061ead41bae8b6719a218985b81c572c6a6f39
benjaminmaddux-eng <benjamin.maddux@ucas-edu.net>
2026-09-08 09:51:46 -0600

    didnt work have to do hoom work

---
+++ idot.prof.py
@@ -0,0 +1,8 @@
+#benjamin maddux ms larsoe 
+firstname=input("put in your fist name")
+while True:
+    try:
+        phonenum=int(input("phone number:   "))
+        otherphone= phonenum.split(3)
+        otherphone= phonenum.split(6)
+
