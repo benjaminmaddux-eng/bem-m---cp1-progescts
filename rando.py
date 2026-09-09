5178c7a4f9103f1d03eed800657e44de6d890399
benjaminmaddux-eng <benjamin.maddux@ucas-edu.net>
2026-09-09 09:33:03 -0600

    plaese

--- rando.py
+++ rando.py
@@ -1,5 +1,5 @@
 import random
 import math
-beans= random.randint(1,22222222222222222222222222222222222222222)
+beans= random.randrange(1,22222222222222222222222222222222222222222,1111111111)
 matb=math.sin(beans)
 print(f"There are {matb} BEANS")
