2765f049e8c8f682a073bbc19337696cc8dac25f
benjaminmaddux-eng <benjamin.maddux@ucas-edu.net>
2026-09-10 09:30:34 -0600

    W

---
+++ roll.py
@@ -0,0 +1,6 @@
+#benjamin maddux la rose 1st period
+print("dice roller")
+sides=int(input("number of sides: "))
+import random
+roll=random.randint(1,sides)
+print(f"this is what you rolled: {roll}")
