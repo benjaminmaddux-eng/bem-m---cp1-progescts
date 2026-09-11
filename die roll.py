ea2ef53d991c6294b9d1a1efb0c6b683eead385f
benjaminmaddux-eng <benjamin.maddux@ucas-edu.net>
2026-09-11 09:07:14 -0600

    yes

--- die roll.py
+++ die roll.py
@@ -1,6 +1,6 @@
 #benjamin maddux la rose 1st period
 print("dice roller")
-sides=int(input("number of sides: "))
+sides=int(input("What size dice would you like to roll? (D4, D6, D8, D10, D12, D20):  "))
 import random
 roll=random.randint(1,sides)
 print(f"this is what you rolled: {roll}")
